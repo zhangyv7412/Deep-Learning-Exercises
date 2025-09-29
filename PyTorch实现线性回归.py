0b37c716dac930a13792d4cb770115fcae296ca7
zhangyv7412 <zhangyv7412@gmail.com>
2025-09-29 11:19:54 +0800

    完成PyTorch线性回归代码,增加注释以提高可读性.

--- PyTorch实现线性回归.py
+++ PyTorch实现线性回归.py
@@ -1,5 +1,32 @@
 import torch
-inputs = torch.rand(100,3)
-weights = torch.tensor([[1.1],[2.2],[3.3]])
-bias = torch.tensor(4,4)
-targets = input @ weights + bias + 0.1 * torch.randn(100,1)
+
+inputs = torch.rand(100,3) #生成训练集
+weights = torch.tensor([[1.1],[2.2],[3.3]]) #初始化权重
+bias = torch.tensor(4.4) #初始化偏置
+#,加入一些误差作为噪音,生成Lable值
+targets = inputs @ weights + bias + 0.1 * torch.randn(100,1) 
+
+#初始化参数
+w = torch.rand((3,1),requires_grad=True)
+b = torch.rand((1,),requires_grad=True)
+epoch = 10000
+lr = 0.003
+
+for i in range(epoch):
+    outputs = inputs @ w + b
+    loss = torch.mean(torch.square(outputs - targets))
+    print('loss:',loss.item())
+
+    loss.backward()
+
+    with torch.no_grad():
+        w -= lr * w.grad
+        b -= lr * b.grad
+
+    w.grad.zero_()
+    b.grad.zero_()
+
+
+print('训练后的权重 w:',w)
+print('训练后的偏置 b:',b)
+
